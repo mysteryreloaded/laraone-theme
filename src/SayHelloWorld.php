66fc4f30812606d085f6e2bb65b24ab6c5ef53f8
mysteryreloaded <ntrifunovicsrb@gmail.com>
2019-09-12 19:09:14 +0200

    Delete all except theme.json

--- src/SayHelloWorld.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace mysteryreloaded\laraonetheme;
-class SayHelloWorld
-{
-    public static function world()
-    {
-        return 'Hello World, Composer!';
-    }
-}
--- tests/HelloWorldTest.php
+++
@@ -1,8 +0,0 @@
-<?php
- 
-require_once __DIR__ . '/../vendor/autoload.php';
- 
-use mysteryreloaded\laraonetheme\SayHelloWorld;
- 
- 
-echo SayHelloWorld::world();
