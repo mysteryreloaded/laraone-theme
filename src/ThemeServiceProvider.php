14482c06a37233295534641408661484cfd47f2b
mysteryreloaded <ntrifunovicsrb@gmail.com>
2019-09-13 18:00:16 +0200

    Move files test case #2

--- src/ThemeServiceProvider.php
+++ src/ThemeServiceProvider.php
@@ -6,15 +6,18 @@ use Illuminate\Support\ServiceProvider;
 
 class ThemeServiceProvider extends ServiceProvider {
 
+    protected $commands = ['mysteryreloaded\laraonetheme\Commands\MoveFiles'];
+
     public function boot() {
-        $this->commands([
-            MoveFiles::class
-        ]);
+        // $this->commands([
+        //     MoveFiles::class
+        // ]);
     }
 
     public function register() {
-        $this->app->bind('theme-files', function() {
-             return new FilePacker();
-        });
+        // $this->app->bind('theme-files', function() {
+        //      return new FilePacker();
+        // });
+        $this->commands($this->commands);
     }
 }
