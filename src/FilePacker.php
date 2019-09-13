65dc9c816f43d2a7daa342b1cce778765e9a018f
mysteryreloaded <ntrifunovicsrb@gmail.com>
2019-09-13 21:25:21 +0200

    Fix small bug

--- src/FilePacker.php
+++ src/FilePacker.php
@@ -18,15 +18,15 @@ class FilePacker
     public static function move()
     {
         // if(rename(realpath(dirname(__FILE__)) . FilePacker::themeCreatePath, realpath(dirname(__FILE__)) . "\ThemeCreateCommand.php")) {
-        rename(realpath(dirname(__FILE__)) . FilePacker::themeCreatePath, "..\app\Commands\ThemeCreateCommand.php");
+        rename(realpath(dirname(__FILE__)) . FilePacker::themeCreatePath, ".\app\Commands\ThemeCreateCommand.php");
 
-        rename(realpath(dirname(__FILE__)) . FilePacker::themePackagePath, "..\app\Commands\ThemePackageCommand.php");
+        rename(realpath(dirname(__FILE__)) . FilePacker::themePackagePath, ".\app\Commands\ThemePackageCommand.php");
 
-        rename(realpath(dirname(__FILE__)) . FilePacker::themeSyncPath, "..\app\Commands\ThemeSyncCommand.php");
+        rename(realpath(dirname(__FILE__)) . FilePacker::themeSyncPath, ".\app\Commands\ThemeSyncCommand.php");
 
-        rename(realpath(dirname(__FILE__)) . FilePacker::themeValidatePath, "..\app\Commands\ThemeValidateCommand.php");
+        rename(realpath(dirname(__FILE__)) . FilePacker::themeValidatePath, ".\app\Commands\ThemeValidateCommand.php");
 
-        rename(realpath(dirname(__FILE__)) . FilePacker::schemasPath, "..\schemas");
+        rename(realpath(dirname(__FILE__)) . FilePacker::schemasPath, ".\schemas");
         
     }
 }
