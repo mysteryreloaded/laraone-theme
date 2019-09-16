b5b589c7e15b58ce5ebd5ab39b4186371c13e20e
mysteryreloaded <ntrifunovicsrb@gmail.com>
2019-09-16 13:57:10 +0200

    Use commands without publication/copying

--- src/FilePacker.php
+++ src/FilePacker.php
@@ -4,10 +4,10 @@ namespace mysteryreloaded\laraonetheme;
 
 class FilePacker
 {
-    const themeCreatePath = "\ThemeCreateCommand.php";
-    const themePackagePath = "\ThemePackageCommand.php";
-    const themeSyncPath = "\ThemeSyncCommand.php";
-    const themeValidatePath = "\ThemeValidateCommand.php";
+    const themeCreatePath = "\Commands\ThemeCreateCommand.php";
+    const themePackagePath = "\Commands\ThemePackageCommand.php";
+    const themeSyncPath = "\Commands\ThemeSyncCommand.php";
+    const themeValidatePath = "\Commands\ThemeValidateCommand.php";
     const schemasPath = "\schemas";
 
     public function __construct()
@@ -17,21 +17,21 @@ class FilePacker
 
     public static function move()
     {
-        if (!file_exists(".\app\Commands\ThemeCreateCommand.php")) {
-            rename(realpath(dirname(__FILE__)) . FilePacker::themeCreatePath, ".\app\Commands\ThemeCreateCommand.php");
-        }
-        if (!file_exists(".\app\Commands\ThemePackageCommand.php")) {
-            rename(realpath(dirname(__FILE__)) . FilePacker::themePackagePath, ".\app\Commands\ThemePackageCommand.php");
-        }
-        if (!file_exists(".\app\Commands\ThemeSyncCommand.php")) {
-            rename(realpath(dirname(__FILE__)) . FilePacker::themeSyncPath, ".\app\Commands\ThemeSyncCommand.php");
-        }
-        if (!file_exists(".\app\Commands\ThemeValidateCommand.php")) {
-            rename(realpath(dirname(__FILE__)) . FilePacker::themeValidatePath, ".\app\Commands\ThemeValidateCommand.php");
-        }
-        if (!file_exists(".\schemas")) {
-            rename(realpath(dirname(__FILE__)) . FilePacker::schemasPath, ".\schemas");
-        }
+        // if (file_exists(".\vendor\mysteryreloaded\laraone-theme\src\Commands\ThemeCreateCommand.php")) {
+        //     rename(realpath(dirname(__FILE__)) . FilePacker::themeCreatePath, ".\app\Commands\ThemeCreateCommand.php");
+        // }
+        // if (file_exists(".\vendor\mysteryreloaded\laraone-theme\src\Commands\ThemePackageCommand.php")) {
+        //     rename(realpath(dirname(__FILE__)) . FilePacker::themePackagePath, ".\app\Commands\ThemePackageCommand.php");
+        // }
+        // if (file_exists(".\vendor\mysteryreloaded\laraone-theme\src\Commands\ThemeSyncCommand.php")) {
+        //     rename(realpath(dirname(__FILE__)) . FilePacker::themeSyncPath, ".\app\Commands\ThemeSyncCommand.php");
+        // }
+        // if (file_exists(".\vendor\mysteryreloaded\laraone-theme\src\Commands\ThemeValidateCommand.php")) {
+        //     rename(realpath(dirname(__FILE__)) . FilePacker::themeValidatePath, ".\app\Commands\ThemeValidateCommand.php");
+        // }
+        // if (file_exists(".\vendor\mysteryreloaded\laraone-theme\src\schemas")) {
+        //     rename(realpath(dirname(__FILE__)) . FilePacker::schemasPath, ".\schemas");
+        // }
     }
 }
-FilePacker::move();
+// FilePacker::move();
